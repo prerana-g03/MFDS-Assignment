ce90e381a7c4f5acf7c6aad1de0634a621f92f2d
prerana-g03 <preranag03@gmail.com>
2026-08-18 09:59:52 +0530

    Add files via upload

---
+++ madd.py
@@ -0,0 +1,33 @@
+
+
+rows = int(input("enter rows: "))
+cols = int(input ("enter cols: "))
+
+print(" enter elements of matrix A:")
+A= []
+for i in range(rows):
+    row= []
+    for j in range(cols):
+        row.append(int(input(f"A[i][j]: ")))
+    A.append(row)
+
+print("Enter elements of matrix B:" )
+B = []
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        row.append(int(input(f"B[i][j]: ")))
+    B.append(row)
+
+C = []
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        row.append(A[i][j] + B[i][j])
+    C.append(row)
+
+print("\nMatrix Addition:")
+for i in range(rows):
+    for j in range(cols):
+        print(C[i][j], end=" ")
+    print()
